9cfc90c5b44f1846b03cc11b798b9e513a7ff415
saguywalker <saguywalker@protonmail.com>
2025-08-31 14:12:09 +0900

    init

---
+++ src/test/scala/myorg/MyAwesomeWebappTests.scala
@@ -0,0 +1,9 @@
+package myorg
+
+class MyAwesomeWebappTests extends munit.FunSuite {
+
+  test("dummy test") {
+    assert(1 == 1)
+  }
+
+}
---
+++ tyrianapp.js
@@ -0,0 +1,5 @@
+import {
+  TyrianApp
+} from './target/scala-3.6.4/myawesomewebapp-fastopt/main.js';
+
+TyrianApp.launch("myapp");
